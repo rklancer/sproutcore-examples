5582e79843e64645c085db43b1159f205fd85fbb
Richard Klancer <rpk@pobox.com>
2010-09-22 17:49:26 -0400

    Initial state of appController

---
+++ examples/apps/hello/controllers/app.js
@@ -0,0 +1,18 @@
+// ==========================================================================
+// Project:   Hello.appController
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Hello */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.Object
+*/
+Hello.appController = SC.ObjectController.create(
+/** @scope Hello.appController.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
---
+++ examples/apps/hello/tests/controllers/app.js
@@ -0,0 +1,15 @@
+// ==========================================================================
+// Project:   Hello.appController Unit Test
+// Copyright: ©2010 My Company, Inc.
+// ==========================================================================
+/*globals Hello module test ok equals same stop start */
+
+module("Hello.appController");
+
+// TODO: Replace with real unit test for Hello.appController
+test("test description", function() {
+  var expected = "test";
+  var result   = "test";
+  equals(result, expected, "test should equal test");
+});
+
