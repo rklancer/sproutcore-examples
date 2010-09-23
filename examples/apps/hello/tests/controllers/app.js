87d90bf873eb285c301300a9f1947f8778752d31
Richard Klancer <rpk@pobox.com>
2010-09-22 22:57:48 -0400

    Test with mock label and checkbox
    "So, why is the mock label stuck at 'Hello, world!' instead of 'Welcome to SproutCore!'?"

--- examples/apps/hello/tests/controllers/app.js
+++ examples/apps/hello/tests/controllers/app.js
@@ -28,3 +28,31 @@ test("appController should respect 'useTraditionalGreeting' setting", function (
   
 });
 
+
+test("when bound to a view, appController should update the view correctly", function () {
+
+  var mockCheckbox = SC.Object.create({
+    valueBinding: 'Hello.appController.useTraditionalGreeting'
+  });
+  
+  var mockLabel = SC.Object.create({
+    valueBinding: 'Hello.appController.greeting'
+  });
+  
+  SC.run(function () {
+    mockCheckbox.set('value', YES);
+  });  
+  equals(mockLabel.get('value'), "Hello, world!", "mock label view should show the traditional greeting when mock checkbox is checked");
+  
+  SC.run(function () {
+    mockCheckbox.set('value', NO);
+  });
+  equals(mockLabel.get('value'), "Welcome to SproutCore!", "mock label view should show the nontraditional greeting when mock checkbox is unchecked");
+
+  SC.run(function () {
+    mockCheckbox.set('value', YES);
+  });
+  equals(mockLabel.get('value'), "Hello, world!", "mock label view should revert to traditional greeting when mock checkbox is checked again");
+  
+});
+
